8d0aa11dabc703523ae724f07eaa121dcadd3665
s-basta <the.quote.villa@gmail.com>
2024-08-01 12:31:25 +0530

    Initial Setup with HelloWorld
    Basic setup that can be imported into each of our laptops using `Import from Git` within our SpringToolSuite IDEs

---
+++ src/com/training/vloan/helloworld/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.training.vloan.helloworld;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
